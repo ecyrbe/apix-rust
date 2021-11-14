50a87754df3535855ced0547a88d0e2d1c91895a
ecyrbe <ecyrbe@gmail.com>
2021-11-15 00:11:27 +0100

    test(language): DRY tests

--- src/http_utils.rs
+++ src/http_utils.rs
@@ -73,63 +73,33 @@ mod test_get_language {
         reqwest::header::HeaderValue::from_str(value).unwrap(),
       );
     }
+
+    fn from_content_type(value: &str) -> MockHttpHeaders {
+      let mut headers = MockHttpHeaders::new();
+      headers.set_content_type(value);
+      headers
+    }
   }
 
-  // test get language for json MockHttpHeaders
-  #[test]
-  fn test_get_language_for_json_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("application/json");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("json"));
+  // vec intance of HttpHeaders for all test cases
+  fn get_http_test_cases() -> Vec<(MockHttpHeaders, &'static str)> {
+    vec![
+      (
+        MockHttpHeaders::from_content_type("application/json"),
+        "json",
+      ),
+      (MockHttpHeaders::from_content_type("application/xml"), "xml"),
+      (MockHttpHeaders::from_content_type("text/html"), "html"),
+      (MockHttpHeaders::from_content_type("text/css"), "css"),
+      (MockHttpHeaders::from_content_type("text/javascript"), "js"),
+      (MockHttpHeaders::from_content_type("text/plain"), "txt"),
+    ]
   }
-
-  // test get language for xml MockHttpHeaders
+  // test get language for all test cases
   #[test]
-  fn test_get_language_for_xml_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("application/xml");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("xml"));
-  }
-  // test get language for html MockHttpHeaders
-  #[test]
-  fn test_get_language_for_html_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("text/html");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("html"));
-  }
-  // test get language for css MockHttpHeaders
-  #[test]
-  fn test_get_language_for_css_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("text/css");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("css"));
-  }
-  // test get language for javascript MockHttpHeaders
-  #[test]
-  fn test_get_language_for_javascript_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("application/javascript");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("js"));
-  }
-  // test get language for txt MockHttpHeaders
-  #[test]
-  fn test_get_language_for_txt_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("text/plain");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("txt"));
-  }
-  // test get language for other MockHttpHeaders
-  #[test]
-  fn test_get_language_for_other_mock_http_headers() {
-    let mut mock_http_headers = MockHttpHeaders::new();
-    mock_http_headers.set_content_type("application/other");
-    let language = mock_http_headers.get_language();
-    assert_eq!(language, Some("txt"));
+  fn test_get_language() {
+    for (headers, language) in get_http_test_cases() {
+      assert_eq!(headers.get_language(), Some(language));
+    }
   }
 }
