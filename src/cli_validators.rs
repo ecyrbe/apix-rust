8c4921fd233f8f7df71d8bf5d817d0ae8a61d98d
ecyrbe <ecyrbe@gmail.com>
2021-11-15 02:06:26 +0100

    test(validators): add tests

--- src/cli_validators.rs
+++ src/cli_validators.rs
@@ -34,3 +34,25 @@ pub fn validate_param(param: &str, request_type: RequestParam) -> Result<()> {
     ))
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use test_case::test_case;
+  use url::Url;
+
+  // test validate url with test_case
+  #[test_case("https://www.google.com")]
+  #[test_case("http://www.google.com")]
+  #[test_case("ftp://www.google.com" => panics )]
+  fn test_validate_url(url: &str) {
+    assert_eq!(validate_url(url).unwrap(), Url::parse(url).unwrap());
+  }
+
+  // test validate param with test_case
+  #[test_case("name:value")]
+  #[test_case("name-value" => panics)]
+  fn test_validate_param(param: &str) {
+    assert_eq!(validate_param(param, RequestParam::Header).unwrap(), ());
+  }
+}
